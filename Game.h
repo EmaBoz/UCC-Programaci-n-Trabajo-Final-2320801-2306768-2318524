f7902a14e04dfbb7287175c92b54c5b63dda98ff
EmaBoz <166394476+EmaBoz@users.noreply.github.com>
2024-06-14 00:01:01 -0300

    Update Game.h

--- Game.h
+++ Game.h
@@ -10,9 +10,11 @@ class Game {
 private:
     Player player1;
     Player player2;
-
     int turn;
     bool gameOver;
+    bool allShipsSunkPlayer1;
+    bool allShipsSunkPlayer2;
+    Player* winner;
     std::vector<std::pair<std::string, int>> ranking;
 
 public:
@@ -23,13 +25,16 @@ public:
     Game(const char* player1Name, const char* player2Name);
     ~Game();
     void play();
+    bool isGameWon() const;
     void placeShips(Board& board, std::vector<Ship*>& fleet);
-    void saveRanking(const char* file) const;
+    void saveRanking(const char* file) ;
 private:
     void initialiseFleet(std::vector<Ship*>& fleet);
     void playerTurn(Board& enemyBoard, std::vector<Ship*>& enemyFleet);
     void switchTurn();
+    void announceWinner()const;
     void checkGameOver();
+    bool allShipsSunk(const std::vector<Ship*>& fleet) const;
     void updateRanking(const char* winnerName);
 };
 #endif
