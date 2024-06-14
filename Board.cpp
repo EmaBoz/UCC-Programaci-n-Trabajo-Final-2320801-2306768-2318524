6d9e671a6de85b8988d79a5d7d2529a08e5a5352
EmaBoz <166394476+EmaBoz@users.noreply.github.com>
2024-06-14 00:46:09 -0300

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -1,6 +1,5 @@
 #include "Board.h"
 #include <iostream>
-
 Board::Board() {
     for (int i = 0; i < MIN_BOARD_SIZE; ++i) {
         for (int j = 0; j < MIN_BOARD_SIZE; ++j) {
@@ -23,7 +22,25 @@ void Board::print() const {
         std::cout << std::endl;
     }
 }
-
+bool Board::isHit(int row, int col) {
+    if (board[row][col] == 1) {
+        board[row][col] = 2;
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+bool Board::isGameWon() {
+    for (int row = 0; row < 10; row++) {
+        for (int col = 0; col < 10; col++) {
+            if (board[row][col] == 1) {
+                return false; // game is not yet won
+            }
+        }
+    }
+    return true; // game is won
+}
 char Board::getPosition(int row, int column) const {
     return board[row][column];
 }
